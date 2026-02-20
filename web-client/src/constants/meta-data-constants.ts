b62dc2dd06cf7a29b85c6111b04cb68c09cecdc0
Sayem Shafayet <ishafayet@gmail.com>
2026-02-20 20:37:58 +0600

    Add new constant files for crypto, metadata, miscellaneous, and test configurations
    - Introduced `crypto-specs.ts` to define cryptographic constants and specifications.
    - Created `meta-data-constants.ts` for structured metadata constants related to core, origin, and image groups.
    - Added `misc-constants.ts` for various application-wide constants including file sizes and MIME types.
    - Implemented `test-constants.ts` for testing-related constants, including placeholders and test vectors.

---
+++ web-client/src/constants/meta-data-constants.ts
@@ -0,0 +1,19 @@
+export const MetaDataConstant = {
+  CORE_GROUP_NAME: "core",
+  CORE: {
+    MIME_TYPE: "mimeType",
+    SIZE_BEFORE_ENCRYPTION: "sizeBeforeEncryption",
+  },
+  ORIGIN_GROUP_NAME: "origin",
+  ORIGIN: {
+    CLIENT_NAME: "clientName",
+    ORIGINAL_NAME: "originalName",
+    ORIGINATION_SOURCE: "originationSource",
+    ORIGINATION_DATE: "originationDate",
+    LAST_MODIFIED_DURING_ORIGINATION: "lastModifiedDuringOrigination",
+  },
+  IMAGE_GROUP_NAME: "image",
+  IMAGE: {
+    IMAGE_THUMBNAIL_CONTENT: "imageThumbnailContent",
+  },
+};
