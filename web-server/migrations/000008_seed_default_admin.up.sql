bf4f4ce394296203075bbba6a8875e645a08fd4b
Sayem Shafayet <ishafayet@gmail.com>
2026-02-20 17:53:26 +0600

    Enhance Makefile with new migration command and remove obsolete admin migration scripts
    - Added a new `migrate-down-all` command to the Makefile for rolling back all migrations.
    - Removed outdated migration scripts for creating and deleting the default admin user, as this functionality is now handled at application startup.

--- web-server/migrations/000008_seed_default_admin.up.sql
+++
@@ -1,6 +0,0 @@
--- Default admin user is created at application startup by admin_service.go
--- Username: admin
--- Password: PleaseChangeMe@YourEarliest2Day
--- Permissions: MANAGE_ALL_USER=true, CREATE_USER=true, CREATE_BUCKET=true
-SELECT 1;
-
