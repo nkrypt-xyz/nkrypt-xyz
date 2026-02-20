70325e821d84f4598ddf3d99b351fcaddacc8597
Sayem Shafayet <ishafayet@gmail.com>
2026-02-20 20:37:58 +0600

    Add new constant files for crypto, metadata, miscellaneous, and test configurations
    - Introduced `crypto-specs.ts` to define cryptographic constants and specifications.
    - Created `meta-data-constants.ts` for structured metadata constants related to core, origin, and image groups.
    - Added `misc-constants.ts` for various application-wide constants including file sizes and MIME types.
    - Implemented `test-constants.ts` for testing-related constants, including placeholders and test vectors.

---
+++ web-client/src/constants/crypto-specs.ts
@@ -0,0 +1,27 @@
+export const BLOB_API_CRYPTO_META_HEADER_NAME = "nk-crypto-meta";
+
+export const BUCKET_CRYPTO_SPEC = "NK001";
+
+export const IV_LENGTH = 12;
+export const SALT_LENGTH = 16;
+
+export const PASSPHRASE_ENCODING = "utf-8";
+
+export const PASSPHRASE_IMPORTKEY_ALGORITHM = "PBKDF2";
+export const PASSPHRASE_DERIVEKEY_ALGORITHM = "PBKDF2";
+export const PASSPHRASE_DERIVEKEY_ITERATIONS = 100000;
+export const PASSPHRASE_DERIVEKEY_HASH_ALGORITHM = "SHA-256";
+export const PASSPHRASE_DERIVEKEY_GENERATED_KEYLENGTH = 256;
+
+export const ENCRYPTION_ALGORITHM = "AES-GCM";
+export const ENCRYPTION_TAGLENGTH_IN_BITS = 128;
+
+export const BLOB_CHUNK_SIZE_BYTES = 1024 * 1024 - 128 / 8;
+export const BLOB_CHUNK_SIZE_INCLUDING_TAG_BYTES = 1024 * 1024;
+
+export const CryptoConstant = {
+  ALGO_AES_GCM_256: {
+    CONTENT_FILE_HEADER: new Uint8Array([0x4e, 0x4b, 0x30, 0x30, 0x31]),
+    IV_LENGTH: 12,
+  },
+};
