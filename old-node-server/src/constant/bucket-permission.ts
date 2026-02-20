2ce9efe4ec8c232ea097faf227abee574c02d835
Sayem Shafayet <ishafayet@gmail.com>
2026-02-20 18:37:41 +0600

    Remove old Node.js server files and configurations
    - Deleted obsolete server files and documentations

--- old-node-server/src/constant/bucket-permission.ts
+++
@@ -1,9 +0,0 @@
-enum BucketPermission {
-  MODIFY = "MODIFY",
-  MANAGE_AUTHORIZATION = "MANAGE_AUTHORIZATION",
-  DESTROY = "DESTROY",
-  VIEW_CONTENT = "VIEW_CONTENT",
-  MANAGE_CONTENT = "MANAGE_CONTENT",
-}
-
-export { BucketPermission };
--- old-node-server/src/constant/misc-constants.ts
+++
@@ -1,7 +0,0 @@
-const miscConstants = {
-  BUCKET_CREATOR_AUTHORIZATION_MESSAGE: "Created this bucket",
-  BUCKET_NEW_DEFAULT_AUTHORIZATION_MESSAGE_fn: (authorizedByUserName: string) => `Authorized by @${authorizedByUserName}`,
-  SESSION_LIST_ALL_COUNT_LIMIT: 20
-}
-
-export { miscConstants }
