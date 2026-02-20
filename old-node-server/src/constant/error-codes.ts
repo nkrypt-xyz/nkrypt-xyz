c15338ed1c2afac439f015af3317962ab7d3b0ad
Sayem Shafayet <ishafayet@gmail.com>
2026-02-20 18:37:41 +0600

    Remove old Node.js server files and configurations
    - Deleted obsolete server files and documentations

--- old-node-server/src/constant/error-codes.ts
+++
@@ -1,8 +0,0 @@
-enum ErrorCode {
-  // a situation that can only be resolved by developer intervention
-  DEVELOPER_ERROR = "DEVELOPER_ERROR",
-
-  // user, session & authentication
-}
-
-export { ErrorCode };
